dba8517ac3f4a2b63d4fb32574c08a280ca8f620
akhshyganesh <akhshyganeshb@gmail.com>
2026-06-09 20:59:06 +0530

    feat: add MongoDB connection setup and integrate it into the application

--- my-first-backend/index.js
+++ my-first-backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import { run } from './database.js';
 
 const app = express();
 const port = 3000;
 
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World!' });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
