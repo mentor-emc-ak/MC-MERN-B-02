659d37d71c6f525b2635453876963875b20d68dd
akhshyganesh <akhshyganeshb@gmail.com>
2026-06-09 20:59:14 +0530

    feat: implement user and post models; add CRUD operations for users in the API; configure dotenv for environment variables

--- my-first-backend/index.js
+++ my-first-backend/index.js
@@ -1,11 +1,77 @@
 import express from 'express';
-import { run } from './database.js';
+import User from './models/user.js';
+import { connectDB } from './database.js';
 
 const app = express();
 const port = 3000;
 
-run().catch(console.dir);
+connectDB()
 
+app.use(express.json());
+
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.patch('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
